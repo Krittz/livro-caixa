8ba6faf990ddad59e69bd9018c6d649a84196d0b
Cristian Alves <cristianalvessilvak@gmail.com>
2025-05-30 15:38:58 -0300

    Add projeto

---
+++ src/main/java/br/com/cran/livro_caixa/LivroCaixaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cran.livro_caixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivroCaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivroCaixaApplication.class, args);
+	}
+
+}
